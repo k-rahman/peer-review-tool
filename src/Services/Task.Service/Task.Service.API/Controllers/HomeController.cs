38ac1a521df4a0ce1c8626c32537d95f5dad125e
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-14 02:00:22 +0300

    Create HomeController which redirect to Swagger UI

---
+++ src/Services/Task.Service/Task.Service.API/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task.Service.API.Controllers
+{
+  public class HomeController : Controller
+  {
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+      return new RedirectResult("~/swagger");
+    }
+  }
+}
