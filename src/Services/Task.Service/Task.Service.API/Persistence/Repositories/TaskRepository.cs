2e5934c8be69f9ed28f620e1c13ddd2ccf03a48b
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-17 12:50:20 +0300

    Implement "GetById", "Insert" and "Delete" methods

--- src/Services/Task.Service/Task.Service.API/Persistence/Repositories/TaskRepository.cs
+++ src/Services/Task.Service/Task.Service.API/Persistence/Repositories/TaskRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -8,13 +9,35 @@ namespace Task.Service.API.Persistence.Repositories
 {
         public class TaskRepository : BaseRepository, ITaskRepository
         {
+
                 public TaskRepository(TaskContext context) : base(context)
                 {
                 }
 
-                public async Task<IEnumerable<Domain.Models.Task>> ListAsync()
+                public async Task<IEnumerable<Domain.Models.Task>> GetAsync()
                 {
-                        return await _context.Tasks.ToListAsync();
+                        return await _context.Tasks.Include(task => task.Criteria).ToListAsync();
+                }
+
+                public async Task<Domain.Models.Task> GetByIdAsync(Guid Id)
+                {
+                        return await _context.Tasks.Include(task => task.Criteria)
+                                                .SingleOrDefaultAsync(task => task.Id == Id);
+                }
+
+                public async System.Threading.Tasks.Task InsertAsync(Domain.Models.Task task)
+                {
+                        await _context.Tasks.AddAsync(task);
+                }
+
+                public void Update(Domain.Models.Task task)
+                {
+                        _context.Tasks.Update(task);
+                }
+
+                public void Delete(Domain.Models.Task task)
+                {
+                        _context.Tasks.Remove(task);
                 }
         }
 }
