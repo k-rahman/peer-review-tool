abf6d53d01aaf3f71e50eebd294e24428c159ae6
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-17 12:59:34 +0300

    Implement CRUD in controller

--- src/Services/Task.Service/Task.Service.API/Controllers/TaskController.cs
+++ src/Services/Task.Service/Task.Service.API/Controllers/TaskController.cs
@@ -1,116 +1,71 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Task.Service.API.Domain.Services;
 using Task.Service.API.Resources;
 
 namespace Task.Service.API.Controllers
 {
+        [Route("api/v1/tasks")]
+        [ApiController]
+        public class TaskController : ControllerBase
+        {
+                private readonly ITaskService _taskService;
 
-  [Route("api/v1/[controller]")]
-  [ApiController]
-  public class TaskController : ControllerBase
-  {
-    private static readonly List<TaskDto> tasks = new()
-    {
-      new TaskDto(
-        Guid.NewGuid(),
-        "first task", "do that and this",
-        "http://peerReviewTool.com/task/1",
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        1
-      ),
-      new TaskDto(
-        Guid.NewGuid(),
-        "second task", "do those and these",
-        "http://peerReviewTool.com/task/2",
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        2
-      ),
-      new TaskDto(
-        Guid.NewGuid(),
-        "third task", "do this and that",
-        "http://peerReviewTool.com/task/3",
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        3
-      )
-    };
+                public TaskController(ITaskService taskService)
+                {
+                        _taskService = taskService;
+                }
 
-    // GET api/v1/[controller]/tasks
-    [HttpGet]
-    [Route("tasks")]
-    public IEnumerable<TaskDto> GetTasks()
-    {
-      return tasks;
-    }
+                [HttpGet]
+                public async Task<IEnumerable<TaskResource>> GetTasks()
+                {
+                        return await _taskService.GetAsync();
+                }
 
-    [HttpGet("{id}")]
-    public TaskDto GetTaskById(Guid id)
-    {
-      var task = tasks.Where(item => item.Id == id).SingleOrDefault();
-      return task;
-    }
+                [HttpGet("{id}")]
+                public async Task<IActionResult> GetTaskById(Guid id)
+                {
+                        var task = await _taskService.GetByIdAsync(id);
 
-    [HttpPost]
-    public ActionResult createTask(TaskDto taskToCreate)
-    {
-      var task = new TaskDto(
-        Guid.NewGuid(),
-        taskToCreate.Name,
-        taskToCreate.Description,
-        taskToCreate.link,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        DateTimeOffset.Now,
-        3
-      );
-      tasks.Add(task);
+                        if (task == null)
+                                return NotFound();
 
-      return CreatedAtAction(nameof(GetTaskById), new { id = task.Id }, task);
-    }
+                        return Ok(task);
+                }
 
-    [HttpPut("{id}")]
-    public IActionResult UpdateTask(Guid id, TaskDto taskToUpdate)
-    {
-      var task = tasks.Where(task => task.Id == id).SingleOrDefault();
-      var updatedTask = taskToUpdate with
-      {
-        Name = task.Name,
-        Description = task.Description,
-        link = task.link,
-      };
+                [HttpPost]
+                public async Task<IActionResult> createTask(SaveTaskResource resource)
+                {
+                        var result = await _taskService.InsertAsync(resource);
 
-      var index = tasks.FindIndex(task => task.Id == id);
-      tasks[index] = updatedTask;
+                        if (!result.Success)
+                                return BadRequest(result.Message);
 
-      return NoContent();
-    }
+                        return Ok(result.Task);
+                }
 
-    [HttpDelete("{id}")]
-    public IActionResult DeleteTask(Guid id)
-    {
-      var index = tasks.FindIndex(task => task.Id == id);
-      tasks.RemoveAt(index);
+                [HttpPut("{id}")]
+                public async Task<IActionResult> UpdateTask(Guid id, SaveTaskResource resource)
+                {
+                        var result = await _taskService.UpdateAsync(id, resource);
 
-      return NoContent();
-    }
-  }
+                        if (!result.Success)
+                                return NotFound(result.Message);
+
+                        return Ok(result.Task);
+                }
+
+                [HttpDelete("{id}")]
+                public async Task<IActionResult> DeleteTask(Guid id)
+                {
+                        var result = await _taskService.DeleteAsync(id);
+
+                        if (!result.Success)
+                                return NotFound(result.Message);
+
+                        return Ok(new { id = result.Task.Id });
+                }
+        }
 }
