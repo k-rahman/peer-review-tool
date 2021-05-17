1b003e15ddace9c783afe3c44a0062bcf73f819e
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-17 12:52:06 +0300

    Apply IEntityTypeConfiguration configuration

---
+++ src/Services/Task.Service/Task.Service.API/Persistence/Contexts/TaskContext.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Task.Service.API.Persistence.EntityConfiguration;
+
+namespace Task.Service.API.Persistence.Context
+{
+        public class TaskContext : DbContext
+        {
+                public DbSet<Domain.Models.Task> Tasks { get; set; }
+
+                public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+                {
+                }
+
+                protected override void OnModelCreating(ModelBuilder builder)
+                {
+                        builder.HasPostgresExtension("uuid-ossp");
+                        builder.ApplyConfiguration(new TaskEntityTypeConfiguration());
+                        builder.ApplyConfiguration(new CriterionEntityTypeConfiguration());
+                }
+        }
+}
