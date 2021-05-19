35a19b6a4889c3800d6bae7edfa0b570c59c86e6
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-19 23:19:44 +0300

    Add a new action "getTaskByLink"

--- src/Services/Task.Service/Task.Service.API/Domain/Repositories/ITaskRepository.cs
+++ src/Services/Task.Service/Task.Service.API/Domain/Repositories/ITaskRepository.cs
@@ -7,7 +7,8 @@ namespace Task.Service.API.Domain.Repositories
         public interface ITaskRepository
         {
                 Task<IEnumerable<Domain.Models.Task>> GetAsync();
-                Task<Domain.Models.Task> GetByIdAsync(int Id);
+                Task<Domain.Models.Task> GetByIdAsync(int id);
+                Task<Domain.Models.Task> GetByLinkAsync(Guid link);
                 System.Threading.Tasks.Task InsertAsync(Domain.Models.Task task);
                 void Delete(Domain.Models.Task task);
         }
