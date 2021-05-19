da921aa5e464a4a1ace0c1ae9bf9feab41b5e1f1
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-19 09:44:41 +0300

    Add allow cross origin

--- src/Services/Task.Service/Task.Service.API/Startup.cs
+++ src/Services/Task.Service/Task.Service.API/Startup.cs
@@ -22,6 +22,7 @@ namespace Task.Service.API
 {
         public class Startup
         {
+                readonly string AllowSpecificOrigins = "_AllowSpecificOrigins";
                 public Startup(IConfiguration configuration)
                 {
                         Configuration = configuration;
@@ -32,7 +33,6 @@ namespace Task.Service.API
                 // This method gets called by the runtime. Use this method to add services to the container.
                 public void ConfigureServices(IServiceCollection services)
                 {
-
                         services.AddScoped<ITaskService, TaskService>();
                         services.AddScoped<ITaskRepository, TaskRepository>();
                         services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -47,6 +47,15 @@ namespace Task.Service.API
                         {
                                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Task.Service.API", Version = "v1" });
                         });
+
+                        services.AddCors(options =>
+                        {
+                                options.AddPolicy(name: AllowSpecificOrigins,
+                                        builder => builder
+                                                .WithOrigins("http://localhost:3000")
+                                                .AllowAnyMethod()
+                                                .AllowAnyHeader());
+                        });
                 }
 
                 // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,8 +69,9 @@ namespace Task.Service.API
                                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task.Service.API v1"));
                         }
 
-
                         app.UseRouting();
+
+                        app.UseCors(AllowSpecificOrigins);
 
                         app.UseAuthorization();
 
