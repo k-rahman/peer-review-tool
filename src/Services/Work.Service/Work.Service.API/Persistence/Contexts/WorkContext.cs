62906f672f437654b7fd9ddbf22b9a6aefc923d1
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-23 18:43:06 +0300

    Create work and worksDealine tables type configuration and dbcontext

--- src/Services/Work.Service/Work.Service.API/Persistence/Contexts/WorkContext.cs
+++ src/Services/Work.Service/Work.Service.API/Persistence/Contexts/WorkContext.cs
@@ -2,10 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using Work.Service.API.Domain.Models;
 using Work.Service.API.Presistence.EntityConfigurations;
 
-namespace Work.Service.API.Presistence.Contexts
+namespace Work.Service.API.Persistence.Contexts
 {
         public class WorkContext : DbContext
         {
+                public DbSet<Domain.Models.Work> Works { get; set; }
                 public DbSet<WorksDeadline> WorksDealines { get; set; }
                 public WorkContext(DbContextOptions<WorkContext> options) : base(options)
                 {
@@ -16,7 +17,5 @@ namespace Work.Service.API.Presistence.Contexts
                         builder.ApplyConfiguration(new WorkEntityTypeConfiguration());
                         builder.ApplyConfiguration(new WorksDeadlineEntityTypeConfiguration());
                 }
-
-
         }
 }
