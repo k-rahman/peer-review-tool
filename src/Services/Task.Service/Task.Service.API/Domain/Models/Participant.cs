4e87a4257f0bc9a9df6a3eac057efb5be22524fe
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-25 01:06:28 +0300

    Create many to many relationship with tasks

--- src/Services/Task.Service/Task.Service.API/Domain/Models/Participant.cs
+++ src/Services/Task.Service/Task.Service.API/Domain/Models/Participant.cs
@@ -1,12 +1,13 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 namespace Task.Service.API.Domain.Models
 {
         public class Participant
         {
                 public int Id { get; set; }
-                public string FirstName { get; set; }
-                public string LastName { get; set; }
 
-                public int TaskId { get; set; }
-                public Task Task { get; set; }
+                public ICollection<Task> Tasks = new List<Task>();
+
         }
 }
