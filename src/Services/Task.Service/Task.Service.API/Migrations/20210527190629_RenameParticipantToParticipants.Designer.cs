c6ba152921b53f79c76e5f64cb6f0b34adca50a5
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-05-27 22:07:52 +0300

    Rename participant table to participants

--- src/Services/Task.Service/Task.Service.API/Migrations/20210527190629_RenameParticipantToParticipants.Designer.cs
+++ src/Services/Task.Service/Task.Service.API/Migrations/20210527190629_RenameParticipantToParticipants.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Task.Service.API.Persistence.Contexts;
@@ -9,9 +10,10 @@ using Task.Service.API.Persistence.Contexts;
 namespace Task.Service.API.Migrations
 {
     [DbContext(typeof(TaskContext))]
-    partial class TaskContextModelSnapshot : ModelSnapshot
+    [Migration("20210527190629_RenameParticipantToParticipants")]
+    partial class RenameParticipantToParticipants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
