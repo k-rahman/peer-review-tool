c3c079d99d7be4d0c9aa56b754e25ad9c57e4d4c
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-03 13:36:13 +0300

    Create criterion model

---
+++ src/Services/Review.Service/Review.Service.API/Domain/Models/Criterion.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Review.Service.API.Domain.Models
+{
+        public class Criterion
+        {
+                public int Id { get; set; }
+                public string Description { get; set; }
+                public int MaxPoints { get; set; }
+
+                public Guid TaskUid { get; set; }
+
+                public ICollection<Review> Reviews { get; set; } = new List<Review>();
+                public List<Grade> Grades { get; set; } = new List<Grade>();
+        }
+}
