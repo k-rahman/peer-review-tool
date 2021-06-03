4b78020a4013e8a59d1b74da3d660d594b18fb87
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-03 13:58:45 +0300

    Set required annotation

--- src/Services/Task.Service/Task.Service.API/Resources/SaveCriterionResource.cs
+++ src/Services/Task.Service/Task.Service.API/Resources/SaveCriterionResource.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Task.Service.API.Resources
 {
         public record SaveCriterionResource
         {
+                [Required]
                 public string Description { get; set; }
+
+                [Required]
                 public int MaxPoints { get; set; }
         }
 }
