a52ebd379c59a0d8a727595356cfec189333e7f4
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-03 13:43:53 +0300

    Mirgration scripts

--- src/Services/Task.Service/Task.Service.API/Migrations/20210531164916_AddAuth0IdColumnToParticipants.Designer.cs
+++ src/Services/Task.Service/Task.Service.API/Migrations/20210531164916_AddAuth0IdColumnToParticipants.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Task.Service.API.Persistence.Contexts;
@@ -9,9 +10,10 @@ using Task.Service.API.Persistence.Contexts;
 namespace Task.Service.API.Migrations
 {
     [DbContext(typeof(TaskContext))]
-    partial class TaskContextModelSnapshot : ModelSnapshot
+    [Migration("20210531164916_AddAuth0IdColumnToParticipants")]
+    partial class AddAuth0IdColumnToParticipants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
