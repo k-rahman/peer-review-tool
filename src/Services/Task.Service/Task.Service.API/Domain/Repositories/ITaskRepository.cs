c1033202e284840435e4786383846c21e66ff306
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-03 13:43:21 +0300

    Add 2 new methods to get tasks by instructor or participant

--- src/Services/Task.Service/Task.Service.API/Domain/Repositories/ITaskRepository.cs
+++ src/Services/Task.Service/Task.Service.API/Domain/Repositories/ITaskRepository.cs
@@ -7,8 +7,8 @@ namespace Task.Service.API.Domain.Repositories
         public interface ITaskRepository
         {
                 Task<IEnumerable<Domain.Models.Task>> GetAsync();
-                Task<IEnumerable<Domain.Models.Task>> GetByInstructorIdAsync(int id);
-                Task<IEnumerable<Domain.Models.Task>> GetByParticipantIdAsync(int id);
+                Task<IEnumerable<Domain.Models.Task>> GetByInstructorIdAsync(string id);
+                Task<IEnumerable<Domain.Models.Task>> GetByParticipantIdAsync(string id);
                 Task<Domain.Models.Task> GetByIdAsync(int id);
                 Task<Domain.Models.Task> GetByUidAsync(Guid uid);
                 System.Threading.Tasks.Task InsertAsync(Domain.Models.Task task);
