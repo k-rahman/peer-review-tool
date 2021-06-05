af488f0672b4451d7bd451aaf2541051514fb5e4
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-05 10:54:37 +0300

    Create login, logout and signup components

---
+++ src/Web/src/components/LoginButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+	const { loginWithRedirect } = useAuth0();
+	return (
+		<Button
+			color="inherit"
+			onClick={() => loginWithRedirect({})}>
+			Log In
+		</Button>
+	);
+};
+
+export default LoginButton;
---
+++ src/Web/src/components/SignupButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const SignupButton = () => {
+	const { loginWithRedirect } = useAuth0();
+	return (
+		<Button
+			color="inherit"
+			onClick={() => loginWithRedirect({
+				screen_hint: "signup",
+			})}>
+			Sign Up
+		</Button>
+	);
+};
+
+export default SignupButton;
