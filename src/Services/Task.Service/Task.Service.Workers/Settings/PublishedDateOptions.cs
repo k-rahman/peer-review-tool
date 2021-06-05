38953e6f21496713f12c328bee64f56ebe160f4f
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-05 22:17:21 +0300

    Create option classes for configuration settings and register services in DI container

---
+++ src/Services/Task.Service/Task.Service.Workers/Settings/PublishedDateOptions.cs
@@ -0,0 +1,9 @@
+namespace Task.Service.Workers.Settings
+{
+        public class PublishedDateOptions
+        {
+                public const string CheckTimes = "CheckTimes";
+
+                public int CheckTime { get; set; }
+        }
+}
