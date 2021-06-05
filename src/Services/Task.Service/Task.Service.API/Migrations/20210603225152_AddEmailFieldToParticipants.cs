96551fe836b8a65481ed25f3077b4d30d8bd7fdb
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-05 10:59:03 +0300

    Migration scripts

---
+++ src/Services/Task.Service/Task.Service.API/Migrations/20210603225152_AddEmailFieldToParticipants.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task.Service.API.Migrations
+{
+    public partial class AddEmailFieldToParticipants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "participants",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "participants");
+        }
+    }
+}
