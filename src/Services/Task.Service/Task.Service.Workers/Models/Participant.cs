8968adc001409f7977c98a9e2cc8dda60baba2c0
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-08 14:51:23 +0300

    Create models in workers service to hold workshop dataresult

--- src/Services/Task.Service/Task.Service.Workers/Models/Participant.cs
+++ src/Services/Task.Service/Task.Service.Workers/Models/Participant.cs
@@ -2,7 +2,7 @@ namespace Task.Service.Workers.Models
 {
         public class Participant
         {
-                public string auth0Id { get; set; }
-                public string email { get; set; }
+                public string Auth0Id { get; set; }
+                public string Email { get; set; }
         }
 }
--- src/Services/Task.Service/Task.Service.Workers/Models/Workshop.cs
+++ src/Services/Task.Service/Task.Service.Workers/Models/Workshop.cs
@@ -5,9 +5,9 @@ namespace Task.Service.Workers.Models
 {
         public class Workshop
         {
-                public int id { get; set; }
+                public int Id { get; set; }
                 public Guid Uid { get; set; }
                 public string InstructorId { get; set; }
-                public ICollection<Participant> Participants { get; set; } = new List<Participant>();
+                public ICollection<Participant> Participants { get; set; }
         }
 }
