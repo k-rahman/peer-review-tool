585d777b84872a8b9fd265b4695674034d50fb98
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-08 14:59:22 +0300

    Create model and mapping from event to model

---
+++ src/Services/Emailing.Service/Emailing.Service.API/Models/Participant.cs
@@ -0,0 +1,8 @@
+namespace Emailing.Service.API.Models
+{
+        public class Participant
+        {
+                public string Auth0Id { get; set; }
+                public string Email { get; set; }
+        }
+}
