85df69e414d23cb721a05ed49d6f466a69235076
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-13 14:41:48 +0300

    Add migrations scripts

---
+++ src/Services/Review.Service/Review.Service.API/Migrations/20210613113738_RenameTaskToWorkshopInCriteria.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Review.Service.API.Migrations
+{
+    public partial class RenameTaskToWorkshopInCriteria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "task_uid",
+                table: "criteria",
+                newName: "workshop_uid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "reviewer_id",
+                table: "reviews",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "workshop_uid",
+                table: "criteria",
+                newName: "task_uid");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "reviewer_id",
+                table: "reviews",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
