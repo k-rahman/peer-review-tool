8a5cd85df0c2aea57498b01cb4ddf49292762970
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-06-24 22:32:54 +0300

    Add review, submission services api operations

---
+++ src/Web/src/api/reviews.js
@@ -0,0 +1,16 @@
+import api from "./api";
+
+const endpoint = "r/api/v1/reviews";
+
+const getReviews = workshopUid => api.get(`${endpoint}/${workshopUid}`);
+
+// const getGrades = reviewId => api.get(`${endpoint}/grades/${reviewId}`);
+
+const updateReview = (reviewId, review) =>
+  api.put(`${endpoint}/${reviewId}`, review);
+
+export default {
+  getReviews,
+  getGrades,
+  updateReview,
+};
