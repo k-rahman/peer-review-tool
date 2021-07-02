901fc6c1f5bbef587f73619979da4d49cf3a6af6
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-03 00:32:20 +0300

    Create a workshop header component

---
+++ src/Web/src/components/WorkshopsHeader.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Typography, Fab, makeStyles } from "@material-ui/core";
+import { Add as PlusIcon } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+	root: {
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "flex-end",
+		textAlign: "left",
+		padding: [[18, 38]]
+
+	}
+});
+
+const WorkshopsHeader = ({ handleAddClick, isInstructor }) => {
+	const classes = useStyles();
+
+	return (
+		<Typography variant="h4" className={classes.root} component="h1">
+			Workshops
+			{isInstructor &&
+				<span>
+					<Fab color="secondary" aria-label="add" onClick={handleAddClick}>
+						<PlusIcon />
+					</Fab>
+				</span>
+			}
+		</Typography>
+	);
+}
+
+export default WorkshopsHeader;
