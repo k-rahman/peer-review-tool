e9db77da1e4569b08130d25dbbe01bb7d92bbcf3
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-03 11:33:54 +0300

    Fix services after taking envoy into use

--- src/Web/src/api/reviews.js
+++ src/Web/src/api/reviews.js
@@ -4,7 +4,7 @@ const endpoint = "r/api/v1/reviews";
 
 const getReviews = workshopUid => api.get(`${endpoint}/${workshopUid}`);
 
-// const getGrades = reviewId => api.get(`${endpoint}/grades/${reviewId}`);
+const getGrades = reviewId => api.get(`${endpoint}/grades/${reviewId}`);
 
 const updateReview = (reviewId, review) =>
   api.put(`${endpoint}/${reviewId}`, review);
