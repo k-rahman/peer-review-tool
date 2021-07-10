30d2fc1aec3dd4bb3d50dea5b62f08dbc6567e32
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-10 10:59:51 +0300

    Add number of reviews and instructor (name) columns to model and db

---
+++ src/Services/Workshop.Service/Workshop.Service.API/Migrations/20210615183535_AddNumberOfReviewsColumn.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Workshop.Service.API.Migrations
+{
+    public partial class AddNumberOfReviewsColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "number_of_reviews",
+                table: "workshops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "number_of_reviews",
+                table: "workshops");
+        }
+    }
+}
