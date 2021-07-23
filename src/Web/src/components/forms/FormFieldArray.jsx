851af4fd182dc5d27b9f91292a869509f800ab1b
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-23 22:29:12 +0300

    Add styles

--- src/Web/src/components/forms/FormFieldArray.jsx
+++ src/Web/src/components/forms/FormFieldArray.jsx
@@ -1,39 +1,53 @@
 import React from "react";
 import { useFormikContext, FieldArray } from "formik";
 import { Button, IconButton } from "@material-ui/core";
-import { Close } from "@material-ui/icons";
+import { Close as CloseIcon } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core";
 
-const FormFieldArray = ({ children, name, value }) => {
+const useStyles = makeStyles({
+	main: {
+		display: "flex",
+		// marginTop: 10,
+	},
+	removeBtn: {
+		alignItems: "flex-start",
+		marginLeft: 8,
+	},
+	addBtn: {
+		display: "flex",
+		justifyContent: "flex-end",
+		flexGrow: 1,
+		padding: [[14, 0]],
+		textAlign: "right"
+	}
+})
 
+const FormFieldArray = ({ children, name, value, addButtonText }) => {
+	const classes = useStyles();
 	const { values } = useFormikContext();
 
 	return (
 		<FieldArray name={name}>
 			{({ remove, push }) => (
-				<div>
+				<>
 					{values[name].map((_, index) => (
-						<div key={index} style={{ display: "flex", justifyContent: "space-between" }}>
+						<div key={index} className={classes.main}>
 							{children(index)}
-							{index <= 0 ?
-								<div style={{ marginRight: 50 }}></div>// show remove button starting from the second field
-								:
-								<IconButton
-									onClick={() => remove(index)}
-									style={{ paddingTop: 56, paddingRight: 12 }}
-								>
-									<Close color="secondary" />
-								</IconButton>
-							}
+							<IconButton className={classes.removeBtn} onClick={() => remove(index)} disabled={index <= 0}>
+								<CloseIcon color={index <= 0 ? "disabled" : "secondary"} />
+							</IconButton>
 						</div>
 					))}
-					<Button
-						variant="contained" color="primary"
-						onClick={() => push(value)}
-						style={{ margin: 30 }}
-					>
-						Add Criterion
-							</Button>
-				</div>
+
+					<div className={classes.addBtn}>
+						<Button
+							variant="contained" color="primary"
+							onClick={() => push(value)}
+						>
+							{addButtonText}
+						</Button>
+					</div>
+				</>
 			)}
 		</FieldArray>
 	);
