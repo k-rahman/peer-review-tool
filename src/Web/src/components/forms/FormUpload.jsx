d4b76c5a031e599c45b19ad859be87ff91393f33
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-23 22:29:42 +0300

    Add styles and use FormField instead of TextField

--- src/Web/src/components/forms/FormUpload.jsx
+++ src/Web/src/components/forms/FormUpload.jsx
@@ -1,49 +1,50 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useFormikContext } from "formik";
-import { Button, TextField } from "@material-ui/core";
-import _ from "lodash";
-import styles from "../../assets/styles/form-field.module.css";
+import { Button, makeStyles } from "@material-ui/core";
 
-const FormUpload = ({ name, title, color, variant }) => {
-	const { values, setFieldValue, errors, touched } = useFormikContext();
-	const [textFieldValue, setTextFieldValue] = useState("");
+import FormField from './FormField';
+
+const withStyles = makeStyles({
+	input: {
+		display: "none"
+	},
+	uploadBtn: {
+		width: "100%",
+		padding: [[14, 0]],
+		textAlign: "right"
+	}
+
+});
+
+const FormUpload = ({ name, title }) => {
+	const classes = withStyles();
+	const { setFieldValue } = useFormikContext();
 
 	const handleChange = e => {
 		const file = e.currentTarget.files[0];
-		console.log(file);
 		setFieldValue(name, file);
-		setTextFieldValue(file.name);
-
-		// const reader = new FileReader();
-
-		// reader.readAsDataURL(file);
-		// let base64String;
-
-		// reader.onload = await function (event) {
-		// 	base64String = event.target?.result
-		// 	setFieldValue(name, base64String);
-		// }
 	}
-
-	console.log(values)
 
 	return (
 		<>
-			<input id="participants-upload" type="file" accept=".csv" onChange={handleChange} name={name} />
+			<input
+				id="participants-upload"
+				type="file"
+				accept=".csv"
+				onChange={handleChange}
+				name={name}
+				className={classes.input}
+			/>
 
-			<div style={{ display: 'flex' }}>
-				<TextField
-					className={`${styles["form-field"]} ${styles["upload-field"]}`}
-					error={_.get(errors, name) && _.get(touched, name)}
-					disabled
-					helperText={_.get(touched, name) && _.get(errors, name)}
-					label="csv file"
-					value={textFieldValue}
-					variant="standard"
-				/>
+			<FormField
+				isDisabled={true}
+				label="csv file"
+				name={name}
+			/>
 
-				<label htmlFor="participants-upload" style={{ alignSelf: "flex-end" }}>
-					<Button variant={variant} color={color} component="span">
+			<div className={classes.uploadBtn}>
+				<label htmlFor="participants-upload">
+					<Button variant="contained" color="primary" component="span">
 						{title}
 					</Button>
 				</label>
