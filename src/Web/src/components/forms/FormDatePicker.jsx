3f3ace1466babfe86c7a6d673106e58f3e82326c
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-23 22:27:32 +0300

    Add an extra condition for validateOnBlur, target field must have an error and is touched

--- src/Web/src/components/forms/FormDatePicker.jsx
+++ src/Web/src/components/forms/FormDatePicker.jsx
@@ -2,47 +2,35 @@ import React from "react";
 import _ from "lodash";
 import { useFormikContext } from "formik";
 import DateFnsUtils from "@date-io/date-fns";
-import { KeyboardDateTimePicker, MuiPickersUtilsProvider, DateTimePicker } from '@material-ui/pickers';
-import { makeStyles } from "@material-ui/core";
-
-const useStyles = makeStyles({
-	root: {
-		width: "100%",
-		marginTop: 38
-
-	},
-});
-
+import { KeyboardDateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 const FormDatePicker = ({ name, label }) => {
-	const classes = useStyles();
+  const { setFieldTouched, errors, touched, setFieldValue, values } = useFormikContext();
 
-	const { setFieldTouched, errors, touched, setFieldValue, values } = useFormikContext();
+  const validateOnBlur = () => {
+    _.get(errors, name) && setFieldTouched(name, true);
+  }
 
-	const validateOnBlur = () => {
-		setFieldTouched(name, true)
-	}
-
-	return (
-		<MuiPickersUtilsProvider utils={DateFnsUtils} >
-			<KeyboardDateTimePicker
-				className={classes.root}
-				disablePast
-				error={_.get(errors, name) && _.get(touched, name)}
-				format="dd.MM.yyyy    hh:mm a"
-				helperText={_.get(touched, name) && _.get(errors, name)}
-				hideTabs
-				inputVariant="outlined"
-				label={label}
-				name={name}
-				onBlur={input => input.target.onblur = validateOnBlur()}
-				onChange={date => setFieldValue(name, date, true)}
-				placeholder="dd.mm.yyyy hh:mm"
-				value={_.get(values, name)}
-				required
-			/>
-		</MuiPickersUtilsProvider>
-	);
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils} >
+      <KeyboardDateTimePicker
+        disablePast
+        error={_.get(errors, name) && _.get(touched, name)}
+        format="dd.MM.yyyy    hh:mm a"
+        fullWidth
+        helperText={_.get(touched, name) && _.get(errors, name)}
+        hideTabs
+        inputVariant="outlined"
+        label={label}
+        name={name}
+        onBlur={input => input.target.onblur = validateOnBlur()}
+        onChange={date => setFieldValue(name, date, true)}
+        placeholder="dd.mm.yyyy hh:mm"
+        required
+        value={_.get(values, name)}
+      />
+    </MuiPickersUtilsProvider>
+  );
 }
 
 export default FormDatePicker;
