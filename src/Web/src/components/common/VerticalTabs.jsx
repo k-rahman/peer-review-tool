45b0f67b57d301324a1ef10a5f337ada20d625af
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-23 22:31:15 +0300

    Add an error check for vertical tabs

--- src/Web/src/components/common/VerticalTabs.jsx
+++ src/Web/src/components/common/VerticalTabs.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { makeStyles, Tab, Tabs as MaterialTabs } from "@material-ui/core";
+import { Error as ErrorIcon } from "@material-ui/icons";
+import { useFormikContext } from 'formik';
+import _ from 'lodash';
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		overflow: "visible",
-	},
 	tabs: {
 		borderRight: `1px solid ${theme.palette.divider}`,
-		paddingTop: 30,
+		paddingTop: 16,
 	},
 }), { name: 'MuiTabs' });
 
 const VerticalTabs = ({ value, handleChange, tabs }) => {
 	const classes = useStyles();
+	const { errors, touched } = useFormikContext();
 
 	const a11yProps = (index) => {
 		return {
@@ -21,16 +22,41 @@ const VerticalTabs = ({ value, handleChange, tabs }) => {
 		};
 	}
 
+
+	const checkForErrors = tab => {
+		for (const field in errors) { // get field name that has an error  ex:("name", "description" ...)
+			for (const label in tab) {
+				const fields = tab[label];  // get fields from tab
+
+				if (Array.isArray(errors[field])) { // if field value is an array as in ("Criteria")
+					for (const item in errors[field])  // get the index of the field in the array
+						if (_.get(touched, `${field}.${item}`) && fields.includes(field))  // if the field is touched && exists in the current tab
+							return true
+				}
+
+				else if (_.get(touched, field) && fields.includes(field)) // if field is touched && exists in the current tab
+					return true;
+			}
+		}
+	}
+
 	return (
 		<MaterialTabs
-			orientation="vertical"
-			variant="scrollable"
-			value={value}
-			onChange={handleChange}
 			aria-label="Vertical tabs example"
 			className={classes.tabs}
+			onChange={handleChange}
+			orientation="vertical"
+			value={value}
+			variant="scrollable"
 		>
-			{tabs.map((label, index) => <Tab key={index} label={label} {...a11yProps(index)} />)}
+			{tabs.map((tab, index) => (
+				<Tab
+					key={index}
+					icon={checkForErrors(tab) && <ErrorIcon color="secondary" fontSize="small" />}
+					label={Object.keys(tab)}
+					{...a11yProps(index)}
+				/>
+			))}
 		</MaterialTabs>
 	);
 }
