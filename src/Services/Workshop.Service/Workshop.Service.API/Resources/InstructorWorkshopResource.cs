c6e95201a192004ae365587cd34fc280a4f7aa2e
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-27 23:36:16 +0300

    Modify Participants field in instructorWorkshopResource to return a full participantResource object

--- src/Services/Workshop.Service/Workshop.Service.API/Resources/InstructorWorkshopResource.cs
+++ src/Services/Workshop.Service/Workshop.Service.API/Resources/InstructorWorkshopResource.cs
@@ -7,6 +7,6 @@ namespace Workshop.Service.API.Resources
 	{
 		public DateTimeOffset Created { get; set; }
 		public DateTimeOffset? Modified { get; set; }
-		public IEnumerable<string> Participants { get; set; } = new List<string>();
+		public IEnumerable<ParticipantResource> Participants { get; set; } = new List<ParticipantResource>();
 	}
 }
