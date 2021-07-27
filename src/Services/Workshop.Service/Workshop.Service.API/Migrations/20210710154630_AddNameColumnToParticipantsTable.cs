cd8f85d25a2cd4780e3e72a537c4ffe9f3d6122a
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-27 23:34:46 +0300

    Add name column to participants

---
+++ src/Services/Workshop.Service/Workshop.Service.API/Migrations/20210710154630_AddNameColumnToParticipantsTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Workshop.Service.API.Migrations
+{
+    public partial class AddNameColumnToParticipantsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "participants",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "participants");
+        }
+    }
+}
