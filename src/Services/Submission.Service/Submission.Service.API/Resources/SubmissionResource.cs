c4eb63587613a42721ff1569ffcef380754ea540
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-07-28 16:36:56 +0300

    Remove SubmissionStart & SubmissionEnd and Add authorId

--- src/Services/Submission.Service/Submission.Service.API/Resources/SubmissionResource.cs
+++ src/Services/Submission.Service/Submission.Service.API/Resources/SubmissionResource.cs
@@ -5,9 +5,8 @@ namespace Submission.Service.API.Resources
         public record SubmissionResource
         {
                 public int Id { get; set; }
+                public string authorId { get; set; }
                 public string Content { get; set; }
-                public DateTimeOffset SubmissionStart { get; set; }
-                public DateTimeOffset SubmissionEnd { get; set; }
                 public DateTimeOffset? Submitted { get; set; }
                 public DateTimeOffset? Modified { get; set; }
         }
