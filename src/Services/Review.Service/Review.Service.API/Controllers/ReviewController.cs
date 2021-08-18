65a58a108f7de7d1256d885f0741632a1b3e2b1b
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-08-18 10:52:55 +0300

    Create review endpoint

--- src/Services/Review.Service/Review.Service.API/Controllers/ReviewController.cs
+++ src/Services/Review.Service/Review.Service.API/Controllers/ReviewController.cs
@@ -1,45 +1,115 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Review.Service.API.Domain.Services;
 using Review.Service.API.Resources;
 
 namespace Review.Service.API.Controller
 {
-        [Route("/api/v1/reviews")]
-        [ApiController]
-        public class ReviewController : ControllerBase
-        {
-                private readonly IReviewService _reviewService;
+	[Route("/api/v1/reviews")]
+	[ApiController]
+	public class ReviewController : ControllerBase
+	{
+		private readonly IReviewService _reviewService;
+		private readonly IReviewDeadlinesService _reviewDeadlinesService;
+		private readonly IGradeService _gradeService;
 
-                public ReviewController(IReviewService reviewService)
-                {
-                        _reviewService = reviewService;
+		public ReviewController(
+			IReviewService reviewService,
+			IReviewDeadlinesService reviewDeadlinesService,
+			IGradeService gradeService
+			)
+		{
+			_reviewService = reviewService;
+			_reviewDeadlinesService = reviewDeadlinesService;
+			_gradeService = gradeService;
 
-                }
+		}
 
-                [HttpGet]
-                public async Task<IActionResult> GetReviews(Guid workshopUid, string reviewerId)
-                {
-                        var reviews = await _reviewService.GetParticipantReviewsByWorkshopUidAsync(workshopUid, reviewerId);
+		[HttpGet("deadlines/{workshopUid}")]
+		public IActionResult GetReviewDeadlines(Guid workshopUid)
+		{
+			var deadlines = _reviewDeadlinesService.GetReviewDeadlines(workshopUid);
 
-                        if (reviews.Count() < 1)
-                                return NotFound();
+			if (deadlines == null)
+				return NotFound();
 
-                        return Ok(reviews);
-                }
+			return Ok(deadlines);
+		}
 
-                [HttpPost]
-                public async Task<IActionResult> SubmitReview(SaveGradeResource grade)
-                {
-                        return Ok();
-                }
+		[HttpGet("{workshopUid}")]
+		[Authorize(Roles = "Participant")]
+		public async Task<ActionResult> GetReviews(Guid workshopUid)
+		{
+			if (!User.IsInRole("Participant"))
+				return Unauthorized();
 
-                [HttpPut]
-                public async Task<IActionResult> EditReview(SaveGradeResource grade)
-                {
-                        return Ok();
-                }
-        }
+			var userId = User.Identity.Name;
+			var reviews = await _reviewService.GetParticipantReviewsByWorkshopUidAsync(workshopUid, userId);
+
+			if (reviews == null)
+				return NotFound();
+
+			return Ok(reviews);
+
+			// if (User.IsInRole("Instructor"))
+			// 	var reviews = await _reviewService.GetInstructorReviewsSummaryByWorkshopUidAsync(workshopUid, userId);
+
+			// else if (User.IsInRole("Participant"))
+
+		}
+
+		[HttpGet("summary/{workshopUid}")]
+		[Authorize(Roles = "Participant, Instructor")]
+		public async Task<ActionResult> GetReviewsSummary(Guid workshopUid)
+		{
+			var userId = User.Identity.Name;
+
+			if (User.IsInRole("Instructor"))
+			{
+				var reviews = await _reviewService.GetInstructorReviewsSummaryByWorkshopUidAsync(workshopUid, userId);
+
+				if (reviews == null)
+					return NotFound();
+
+				return Ok(reviews);
+			}
+			else if (User.IsInRole("Participant"))
+			{
+				var reviews = await _reviewService.GetParticipantReviewsSummaryByWorkshopUidAsync(workshopUid, userId);
+				if (reviews == null)
+					return NotFound();
+
+				return Ok(reviews);
+			}
+
+			return Unauthorized();
+		}
+
+		[HttpGet("grades/{reviewId}")]
+		[Authorize(Roles = "Participant, Instructor")]
+		public async Task<ActionResult<GradesResource>> GetGrades(int reviewId)
+		{
+			var grades = await _gradeService.GetGradesByReviewId(reviewId);
+
+			if (grades == null)
+				return NotFound();
+
+			return Ok(grades);
+		}
+
+		[HttpPut("{reviewId}")]
+		public async Task<IActionResult> EditReview(int reviewId, SaveReviewResource grades)
+		{
+			var result = await _reviewService.UpdateReviewGrades(reviewId, grades);
+
+			if (!result.Success)
+				return NotFound();
+
+			return Ok(result._review);
+		}
+	}
 }
