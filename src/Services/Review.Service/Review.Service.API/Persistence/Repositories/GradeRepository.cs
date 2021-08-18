9d3e60139404eff57d6afeefaa62200760e8b7be
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-08-18 11:12:56 +0300

    Add GradeRepository function

--- src/Services/Review.Service/Review.Service.API/Persistence/Repositories/GradeRepository.cs
+++ src/Services/Review.Service/Review.Service.API/Persistence/Repositories/GradeRepository.cs
@@ -8,24 +8,24 @@ using Review.Service.API.Persistence.Contexts;
 
 namespace Review.Service.API.Persistence.Repositories
 {
-	public class GradeRepository : BaseRepository, IGradeRepository
-	{
+        public class GradeRepository : BaseRepository, IGradeRepository
+        {
 
-		public GradeRepository(ReviewContext context) : base(context)
-		{
-		}
+                public GradeRepository(ReviewContext context) : base(context)
+                {
+                }
 
-		public async Task<IEnumerable<Grade>> GetGradesByReviewId(int reviewId)
-		{
-			return await _context.Grades
-					.Include(g => g.Criterion)
-					.Where(g => g.ReviewId == reviewId)
-					.ToListAsync();
-		}
+                public async Task<IEnumerable<Grade>> GetGradesByReviewId(int reviewId)
+                {
+                        return await _context.Grades
+                                        .Include(g => g.Criterion)
+                                        .Where(g => g.ReviewId == reviewId)
+                                        .ToListAsync();
+                }
 
-		public async Task InsertAsync(Grade grade)
-		{
-			await _context.Grades.AddAsync(grade);
-		}
-	}
+                public async Task InsertAsync(Grade grade)
+                {
+                        await _context.Grades.AddAsync(grade);
+                }
+        }
 }
