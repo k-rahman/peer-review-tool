3dfbff4c4ae9f90040fa528b599ca30e07678d6d
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-08-18 11:06:46 +0300

    Implement 2 new functions to get instructor & participant review summary

--- src/Services/Review.Service/Review.Service.API/Domain/Services/IReivewService.cs
+++ src/Services/Review.Service/Review.Service.API/Domain/Services/IReivewService.cs
@@ -1,14 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using Review.Service.API.Domain.Services.Communication;
 using Review.Service.API.Resources;
 
 namespace Review.Service.API.Domain.Services
 {
         public interface IReviewService
         {
-                Task<IEnumerable<ReviewResource>> GetParticipantReviewsByWorkshopUidAsync(Guid workshopUid, string participantId);
-                Task<Review.Service.API.Domain.Models.Review> GetSubmissionReviews();
-
+                Task<ReviewsResource> GetParticipantReviewsByWorkshopUidAsync(Guid workshopUid, string participantId);
+                Task<ReviewsSummaryResource> GetInstructorReviewsSummaryByWorkshopUidAsync(Guid workshopUid, string instructorId);
+                Task<ReviewSummaryResource> GetParticipantReviewsSummaryByWorkshopUidAsync(Guid workshopUid, string participantId);
+                Task<ReviewResponse> UpdateReviewGrades(int reviewId, SaveReviewResource grades);
         }
 }
--- src/Services/Review.Service/Review.Service.API/Persistence/Repositories/ReviewRepository.cs
+++ src/Services/Review.Service/Review.Service.API/Persistence/Repositories/ReviewRepository.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Review.Service.API.Domain.Repositories;
 using Review.Service.API.Persistence.Contexts;
+using Review.Service.API.Resources;
 
 namespace Review.Service.API.Persistence.Repositories
 {
@@ -24,9 +25,31 @@ namespace Review.Service.API.Persistence.Repositories
                                 .ToListAsync();
                 }
 
+                public async Task<IEnumerable<Domain.Models.Review>> GetInstructorReviewsSummaryByWorkshopUidAsync(Guid workshopUid)
+                {
+                        return await _context.Reviews
+                                .Include(r => r.Submission)
+                                .Include(r => r.Criteria)
+                                .Where(r => r.Submission.WorkshopUid == workshopUid)
+                                .ToListAsync();
+                }
+
+                public async Task<IEnumerable<Domain.Models.Review>> GetParticipantReviewsSummaryByWorkshopUidAsync(Guid workshopUid, string participantId)
+                {
+                        return await _context.Reviews
+                                .Include(r => r.Submission)
+                                .Include(r => r.Criteria)
+                                .Where(r => r.Submission.WorkshopUid == workshopUid && r.Submission.AuthorId == participantId)
+                                .ToListAsync();
+                }
+
                 public async Task<Domain.Models.Review> GetByIdAsync(int id)
                 {
-                        return await _context.Reviews.FindAsync(id);
+                        return await _context.Reviews
+                                .Include(r => r.Submission)
+                                .Include(r => r.Criteria)
+                                .Where(r => r.Id == id)
+                                .SingleOrDefaultAsync();
                 }
 
                 public async Task InsertAsync(Domain.Models.Review review)
